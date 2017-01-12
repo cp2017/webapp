61f53a116e68d77fa81a4cc784c378ee63f9f8c4
Simon Buchholz <simon.buchholz@xaasbase.com>
2017-01-12 15:54:33 +0100

    Created a consume-microservice component and service. Next we need to connect the consume-service to the contracts

---
+++ src/app/services/consume-microservices/consume-microservices.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {Microservice} from "../entities/microservice";
+import {IpfsService} from "../ipfs/ipfs.service";
+import {EthereumService} from "../ethereum/ethereum.service";
+import {ServiceSubscription} from "../entities/serviceSubscription";
+
+@Injectable()
+export class ConsumeMicroservicesServiceService {
+
+  private consumedServices: Microservice[];
+
+  constructor(private _ipfsService: IpfsService, private _ethereumService: EthereumService) {
+  }
+
+
+  consumeService(microservice:Microservice, consumerPublicKey:string): Promise<ServiceSubscription> {
+    return new Promise((resolve, reject) => {
+      if (this._ipfsService.node != null && this._ethereumService.web3 != null) {
+
+        // 0. Unlock the default ethereum account by calling the unlockAccount function of the ethereum service
+        this._ethereumService.unlockDefaultAccount()
+          .then(result => {
+            console.log("Account unlocked");
+            // TODO do the subscription
+
+          })
+          .catch(err => {
+            console.error("Account unlock error: " + err);
+            reject("Account unlock error: " + err);
+          });
+      } else {
+        reject(new Error("You have to connect to the IPFS and Ethereum networks first!"));
+      }
+    });
+  }
+}
